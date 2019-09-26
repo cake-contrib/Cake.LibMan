bc37c08a09ec82baf9d9cdb7b83383b3fe9e56cd
Louis Fischer <louisfischer@gmail.com>
2019-09-25 21:24:58 -0500

    Added additional Cache Clean Aliases

---
+++ src/Cake.LibMan/LibManCacheCleanAliases.cs
@@ -0,0 +1,104 @@
+﻿using Cake.Core;
+using Cake.Core.Annotations;
+using Cake.LibMan.Cache;
+using System;
+
+namespace Cake.LibMan
+{
+    /// <summary>
+    /// LibMan Cache Clean aliases
+    /// </summary>
+    [CakeAliasCategory("LibMan")]
+    [CakeNamespaceImport("Cake.LibMan.Cache")]
+    public static class LibManCacheCleanAliases
+    {
+        /// <summary>
+        /// letes Clears library cache using the default settings
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <example>
+        /// <code>
+        /// <![CDATA[
+        ///     LibManCacheClean();
+        /// ]]>
+        /// </code>
+        /// </example>
+        [CakeMethodAlias]
+        [CakeAliasCategory("Cache")]
+        public static void LibManCacheClean(this ICakeContext context)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+
+            context.LibManCacheClean(new LibManCacheCleanSettings());
+        }
+
+        /// <summary>
+        /// Clears library cache using the specified configurator <see cref="LibManCacheCleanSettings"/>.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="configurator">The settings configurator.</param>
+        /// <example>
+        /// <para> Clears library cache using the via LibMan using the settings returned by a configurator. ('libman cache clean')</para>
+        /// <code>
+        /// <![CDATA[
+        ///        LibManCacheClean(settings =>
+        ///        {
+        ///            settings
+        ///                .WithProvider(CdnProvider.Default)
+        ///                .WithVerbosity(LibManVerbosity.Detailed)
+        ///                .FromPath(@"c:\myproject");
+        ///        });
+        /// ]]>
+        /// </code>
+        /// </example>
+        [CakeMethodAlias]
+        [CakeAliasCategory("Cache")]
+        public static void LibManCacheClean(this ICakeContext context, Action<LibManCacheCleanSettings> configurator)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+
+            if (configurator == null)
+                throw new ArgumentNullException(nameof(configurator));
+
+            var settings = new LibManCacheCleanSettings();
+            configurator(settings);
+            context.LibManCacheClean(settings);
+        }
+
+        /// <summary>
+        /// Clears library cache using the specified <see cref="LibManCacheCleanSettings"/> settings.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="settings">The settings.</param>
+        /// <example>
+        /// <para>Clears library cache using the specified using the specified settings. ('libman cache clean')</para>
+        /// <code>
+        /// <![CDATA[
+        ///      var settings = new LibManCacheCleanSettings
+        ///      {
+        ///         WorkingDirectory = @"c:\myproject",
+        ///         Verbosity = LibManVerbosity.Detailed,
+        ///         Provider = CdnProvider.Default
+        ///      };
+        ///      LibManCacheClean(settings);
+        /// ]]>
+        /// </code>
+        /// </example>
+        [CakeMethodAlias]
+        [CakeAliasCategory("Cache")]
+        public static void LibManCacheClean(this ICakeContext context, LibManCacheCleanSettings settings)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+
+            if (settings == null)
+                throw new ArgumentNullException(nameof(settings));
+
+            LibManAddinInformation.LogVersionInformation(context.Log);
+            var tool = new LibManCacheCleanTool(context.FileSystem, context.Environment, context.ProcessRunner, context.Tools, context.Log);
+            tool.CacheClean(settings);
+        }
+    }
+}
---
+++ src/Cake.LibMan/LibManUninstallAliases.cs
@@ -0,0 +1,83 @@
+﻿using Cake.Core;
+using Cake.Core.Annotations;
+using Cake.LibMan.Uninstall;
+using System;
+
+namespace Cake.LibMan
+{
+    /// <summary>
+    /// LibMan Uninstall aliases
+    /// </summary>
+    [CakeAliasCategory("LibMan")]
+    [CakeNamespaceImport("Cake.LibMan.Install")]
+    public static class LibManUninstallAliases
+    {
+        /// <summary>
+        /// Uninstalls client side libraries using the settings returned by a configurator.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="configurator">The settings configurator.</param>
+        /// <example>
+        /// <para> Uninstalls client side libraries using the settings ('libman uninstall')</para>
+        /// <code>
+        /// <![CDATA[
+        ///    LibManUninstall(settings =>
+        ///    {
+        ///        settings
+        ///            .SetLibrary("jquery")
+        ///            .WithVerbosity(LibManVerbosity.Detailed)
+        ///            .FromPath(@"c:\myproject");
+        ///    });
+        /// ]]>
+        /// </code>
+        /// </example>
+        [CakeMethodAlias]
+        [CakeAliasCategory("Uninstall")]
+        public static void LibManUninstall(this ICakeContext context, Action<LibManUninstallSettings> configurator)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+
+            if (configurator == null)
+                throw new ArgumentNullException(nameof(configurator));
+
+            var settings = new LibManUninstallSettings();
+            configurator(settings);
+            context.LibManUninstall(settings);
+        }
+
+        /// <summary>
+        /// Uninstalls client side libraries using the specified settings.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="settings">The settings.</param>
+        /// <example>
+        /// <para> Uninstalls client side libraries in a specific working directory ('libman uninstall')</para>
+        /// <code>
+        /// <![CDATA[
+        ///     var settings = new LibManUninstallSettings
+        ///        {
+        ///            WorkingDirectory = @"c:\myproject",
+        ///            Verbosity = LibManVerbosity.Detailed,
+        ///            Library = "jquery"
+        ///        };
+        ///     LibManUninstall(settings);
+        /// ]]>
+        /// </code>
+        /// </example>
+        [CakeMethodAlias]
+        [CakeAliasCategory("Uninstall")]
+        public static void LibManUninstall(this ICakeContext context, LibManUninstallSettings settings)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+
+            if (settings == null)
+                throw new ArgumentNullException(nameof(settings));
+
+            LibManAddinInformation.LogVersionInformation(context.Log);
+            var uninstaller = new LibManUninstaller(context.FileSystem, context.Environment, context.ProcessRunner, context.Tools, context.Log);
+            uninstaller.Uninstall(settings);
+        }
+    }
+}
